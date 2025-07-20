a2681c94d3271e0106db412b3741acba0c83c9ff
Tia <tiagupt03@gmail.com>
2025-07-21 01:54:56 +0530

    Fixed typos

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// document.getElementById("booking-form").addEventListener("submit", function(e) {
-//   e.preventDefault();
-
-//   const form = e.target;
-//   const formData = new FormData(form);
-
-//   fetch("https://script.google.com/macros/s/AKfycbwOvF_PUbB9Mnz5SQUr-VXwSw9AfE3h4P5P1UgvAg1ED6HW1NyDEUYAlKmQ9wVPyx4FBw/exec", {
-//     method: "POST",
-//     body: formData
-//   })
-//   .then(res => res.json())
-//   .then(result => {
-//     if (result.status === "success") {
-//       alert("Booking successful!");
-//       form.reset();
-//     } else {
-//       alert("Error: " + result.message);
-//     }
-//   })
-//   .catch(error => {
-//     alert("Fetch error: " + error.message);
-//   });
-// });
-
 document.querySelectorAll(".booking-form").forEach(form => {
   form.addEventListener("submit", function(e) {
     e.preventDefault(); // Prevent default form submission
